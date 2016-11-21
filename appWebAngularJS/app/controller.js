f54ad6f27b88214492ab01a922f59404dff82729
Adri <link.apd@gmail.com>
2016-11-21 12:24:03 +0100

    AppWeb
    Funciona el registro y el calculo de todos los datos.
    
    Falta conseguir los datos del select.

---
+++ appWebAngularJS/app/controller.js
@@ -0,0 +1,70 @@
+var Tubo = require('./modelo/tubo');
+
+
+// Obtiene todos los objetos Tubo de la base de datos
+exports.getTubo = function (req, res){
+	Tubo.find(
+		function(err, tubo) {
+			if (err)
+				res.send(err);
+					res.json(tubo); // devuelve todas las Tubos en JSON
+				}
+			);
+};
+
+// Guarda un objeto Tubo en base de datos
+exports.setTubo = function(req, res) {
+		// Creo el objeto Tubo
+		diametroForm = document.getElementById("sel1").value();
+		colaForm = document.getElementById("sel2").value();
+		calcEncoladuras = (req.body.accesorios * 2.3).toFixed(2);
+		calcCantidad = ((calcEncoladuras * parseInt(colaForm)) / 100).toFixed(2);
+		calcBotes = (calcCantidad / 1000).toFixed(2);
+
+		Tubo.create(
+			{diametro : parseInt(diametroForm), cola: parseInt(colaForm), accesorios: req.body.accesorios,
+				encoladuras: calcEncoladuras, cantidad: calcCantidad, botes: calcBotes},
+			function(err, tubo) {
+				if (err)
+					res.send(err);
+
+				// Obtine y devuelve todas las tubos tras crear una de ellas
+				Tubo.find(function(err, tubo) {
+				 	if (err)
+				 		res.send(err);
+				 	res.json(tubo);
+				});
+			});
+	};
+
+// Modificamos un objeto Tubo de la base de datos
+exports.updateTubo = function(req, res){
+	Tubo.update( {_id : req.params.tubo_id},
+					{$set:{diametro : req.body.diametro,	cola: req.body.cola, accesorios: req.body.accesorios}},
+					function(err, tubo) {
+						if (err)
+							res.send(err);
+
+				// Obtine y devuelve todas las tubos tras crear una de ellas
+				Tubo.find(function(err, tubo) {
+				 	if (err)
+				 		res.send(err);
+				 	res.json(tubo);
+				});
+			});
+	};
+
+// Elimino un objeto Tubo de la base de Datos
+exports.removeTubo = function(req, res) {
+	Tubo.remove({_id : req.params.tubo_id}, function(err, tubo) {
+		if (err)
+			res.send(err);
+
+			// Obtine y devuelve todas las tubos tras borrar una de ellas
+			Tubo.find(function(err, tubo) {
+				if (err)
+					res.send(err);
+				res.json(tubo);
+			});
+		});
+};
